9610b1037ed703dcbfc7118152d61f8a2431b083
ansarisarim <143504648+ansarisarim@users.noreply.github.com>
2026-07-11 21:47:53 +0530

    Create Print-function.py

---
+++ Print-function.py
@@ -0,0 +1,28 @@
+# dsplay the result in python, we used print funcions
+
+#syntax 1
+print(val1)
+     or
+print(val1, val2, val3n)
+>>> a = 12
+>>> b = 32
+>>> c = 43
+>>> print(a, b, c)
+12 32 43
+
+#syntax 2
+>>> print("hello python student")
+hello python student
+  
+>>> print("hello", 'python', """student""")
+hello python student
+  
+>>> print("hello"+'python'+"""student""")
+hellopythonstudent
+
+>>> print("hello"+ 'python'+ """student""")     <----- no , here
+hellopythonstudent
+
+>>> print("hello"+' python'+""" student""")
+hello python student
+
