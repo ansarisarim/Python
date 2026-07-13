0682b3357f8b79ae776a78574e179ae8ce0e8a54
ansarisarim <143504648+ansarisarim@users.noreply.github.com>
2026-07-13 15:06:56 +0530

    Update Print-function.py

--- Print-function.py
+++ Print-function.py
@@ -9,8 +9,9 @@ print(val1, val2, val3n)
 >>> c = 43
 >>> print(a, b, c)
 12 32 43
-
+------------------------------------------------------------------------------------------------------
 #syntax 2
+This syntax used for print the string message on console.
 >>> print("hello python student")
 hello python student
   
@@ -25,4 +26,54 @@ hellopythonstudent
 
 >>> print("hello"+' python'+""" student""")
 hello python student
+
+---------------------------------------------------------------
+#syntax 3
+----------------------
+print message cum value or value come message.
+
+>>> print("value of a =", a)
+value of a = 12
+
+>>> print(a, "is the value of a")
+12 is the value of a
+
+>>> print("value of a =", a, "value of b =", b, "sum value of a and b =", c)
+value of a = 12 value of b = 32 sum value of a and b = 43
+
+
+>>> print(c, "is the sum of a & b")
+30 is the sum of a & b
+
+print("sum of", a, ",", b, "and", d, "is =", d)
+---------------------------------------------------------------------------------------------------------------------
+#syntax 4
+--------------
+
+>>> print("value of a=",(a))
+value of a= 10
+>>> print("vale of a={}".format(a))
+vale of a=10
+
+
+>>> print("val of a={}, val of b={}, is sum of a & b is {}" .format(a, b, c))
+val of a=10, val of b=20, is sum of a & b is 30
+
+>>> print("vale of a {}, value of b {}, value of c {}".format(30, 20 , 10))
+vale of a 30, value of b 20, value of c 10
+
+lst1=[1, 2, 3, 'qw', 'ew']
+lst2=[6, 7, 3, "wq", "esd"]
+>>> print("data in lst1={}, data in lst2={}".format(lst1, lst2))
+data in lst1=[1, 2, 3, 'qw', 'ew'], data in lst2=[6, 7, 3, 'wq', 'esd']
+
+----------------------------------------------------------------------------------------------------------------------
+#syntax 5
+>>> print("my sno number is {} and student name is {} and marks is {}".format(sno, name, marks))
+my sno number is 10 and student name is rossom and marks is 7.9
+
+>>> print("my sno number is %d and student name is %s and marks is %f" % (sno, name, marks))
+my sno number is 10 and student name is rossom and marks is 7.900000
+
+
 
