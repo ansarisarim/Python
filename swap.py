13515c73eed780914c2f20be3e3438d3edfaaf70
ansarisarim <143504648+ansarisarim@users.noreply.github.com>
2026-07-24 19:25:13 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,15 @@
+a=input("inter the first value of a: ")
+b=input("inter the second value of b: ")
+
+print("-"*50)
+print ("orignal value of a: {}".format(a))
+print("orignal value of b: {}".format(b))
+
+print("-"*50)
+
+k=a
+a=b
+b=k
+print("_"*50)
+print("swap value of a: {}" .format(a))
+print("swap value of b: {}".format(b))
