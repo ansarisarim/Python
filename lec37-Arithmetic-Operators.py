8b6154ac7ab860e3f6e2164d57eac18e37f06c90
ansarisarim <143504648+ansarisarim@users.noreply.github.com>
2026-07-29 23:03:31 +0530

    Update lec37-Arithmetic-Operators.py

--- lec37-Arithmetic-Operators.py
+++ lec37-Arithmetic-Operators.py
@@ -30,3 +30,14 @@ print("\t mul {},{} ={}".format(a,b, a*b))
 print("\t div {},{},={}".format(a, b, a/b))
 print("\t floordiv {},{}={}".format(a, b, a//b))
 print("\t expo {},{}={}".format(a,b, a**b))
+
+------------------------------------------------------------------------------------------------
+
+Arithmetic Operators — Notes
+
++  → Addition → a+b
+-  → Subtraction → a-b
+*  → Multiplication → a*b
+/  → Division → a/b → hamesha float return karta hai (decimal)
+// → Floor Division → a//b → decimal ke baad ka part hata deta hai (round down)
+** → Exponent (power) → a**b → matlab a ka b power (a^b)
