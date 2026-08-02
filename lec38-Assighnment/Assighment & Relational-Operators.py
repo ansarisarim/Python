7e1c584d5306c7965a7b3810e8bb0bb6a4c10d45
ansarisarim <143504648+ansarisarim@users.noreply.github.com>
2026-08-02 14:43:04 +0530

    Update Assighment & Relational-Operators.py

--- lec38-Assighnment/Assighment & Relational-Operators.py
+++ lec38-Assighnment/Assighment & Relational-Operators.py
@@ -1,3 +1,11 @@
+# Single line assighnment aperators.
+>>> a=12
+>>> b=8
+>>> c=a+b
+>>> print(a,b,c)
+12 8 20
+------------------------------------------------------------------------------------------------------------
+
 # Multiline assighnment aperators.
 >>> a,b=10,3
 >>> c,d,e,f=a+b,a/b,a-b,a*b
