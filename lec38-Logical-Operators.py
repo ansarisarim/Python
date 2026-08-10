a4706d9725a2928ef4071917445e8e3c5d93bdcf
ansarisarim <143504648+ansarisarim@users.noreply.github.com>
2026-08-10 16:39:12 +0530

    Update lec38-Logical-Operators.py

--- lec38-Logical-Operators.py
+++ lec38-Logical-Operators.py
@@ -1,2 +1,23 @@
-and Operator
+# logical operators is to combaine two or more relational expressions. 
+
+1) "and" Operator
 --------------------------------
+
+>>> True and True...........True
+>>> False and True..........False
+>>> False and False.........False
+>>> True and False..........False
+
+>>> 10>2 and 30>100.........False
+>>> 10>2 and 30>40...........False
+>>> 10>2 and 30>20...........True  full length evaluation
+>>> 10>2 and 30>40............False 
+>>> 10>2 and 30>20............True
+>>> 10>2 and 30>20 and 30>2.............True   
+>>> 10>2 and 30>20 and 30>200...............False
+>>> 30>300 and 10>2 and 30>20...............False    short circuit evaluation 
+
+Definition:  
+"In AND operator, if the first condition is False, Python stops checking remaining conditions and 
+directly returns False. This is called Short Circuit Evaluation. If all conditions are True, Python checks every
+condition, called Full Length Evaluation."
